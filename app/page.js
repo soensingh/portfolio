8580f56edf12ebc464bf97ade58e6126e68a49b4
Soen Singh <124899734+soensingh@users.noreply.github.com>
2025-09-01 01:48:45 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState, useEffect } from 'react';
 import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import Blog from "./components/homepage/blog";
@@ -10,22 +11,28 @@ import HeroSection from "./components/homepage/hero-section";
 import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 
-async function getData() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-
-  const data = await res.json();
-
-  const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
-
-  return filtered;
-};
-
-export default async function Home() {
-  const blogs = await getData();
+export default function Home() {
+  const [blogs, setBlogs] = useState([]);
+  
+  useEffect(() => {
+    async function fetchBlogs() {
+      try {
+        const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`);
+        
+        if (!res.ok) {
+          throw new Error('Failed to fetch data');
+        }
+        
+        const data = await res.json();
+        const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
+        setBlogs(filtered);
+      } catch (error) {
+        console.error('Error fetching blogs:', error);
+      }
+    }
+    
+    fetchBlogs();
+  }, []);
 
   return (
     <div suppressHydrationWarning >
@@ -35,7 +42,8 @@ export default async function Home() {
       <Skills />
       <Projects />
       <Education />
+      {blogs.length > 0 && <Blog blogs={blogs} />}
       <ContactSection />
     </div>
-  )
-};
+  );
+}
