d0ec4a2980367275824282bbae7e770be3289da3
Soen Singh <124899734+soensingh@users.noreply.github.com>
2025-09-01 01:52:36 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -42,7 +42,6 @@ export default function Home() {
       <Skills />
       <Projects />
       <Education />
-      {blogs.length > 0 && <Blog blogs={blogs} />}
       <ContactSection />
     </div>
   );
